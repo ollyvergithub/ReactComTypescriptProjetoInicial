fc16905530168a249ee80ecf1f100c40b7932492
ollyvergithub <ollyverottoboni@gmail.com>
2023-09-30 11:34:57 -0300

    01 -Criando variaveis em componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Seção 11 React com Typescript</h1>
-    </div>
-  );
+
+    // 1 - Variaveis
+    const name: string = "Ollyver"
+    const age: number = 51
+    const isWorking: boolean = true
+
+    return (
+        <div className="App">
+            <h1>Seção 11 React com Typescript</h1>
+            <h2>Exibindo a variavel nome: {name}</h2>
+            <h2>Exibindo a variavel age: {age}</h2>
+            {isWorking && (
+                <p>Está trabalhando</p>
+            )}
+        </div>
+    );
 }
 
 export default App;
