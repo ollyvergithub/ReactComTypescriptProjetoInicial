f5c445cd42a8da5d8c62c301c32dde1deff80c08
ollyvergithub <ollyverottoboni@gmail.com>
2023-10-07 10:09:02 -0300

    Criando Context arquivo - App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {createContext} from 'react';
 
 // 4 - Importacao de componentes
 import FirstComponent from "./components/FirstComponent";
@@ -16,6 +16,22 @@ type textOrNull = string | null
 type fixed = "Isso" | "Aquilo"
 const testandoFixed: fixed = "Isso"
 
+
+// 9 - context
+const contextValue: IAppContext = {
+    language: "Javascript",
+    framework: "Express",
+    projects: 5,
+}
+
+interface IAppContext {
+    language: string,
+    framework: string,
+    projects: number,
+}
+
+export const AppContext = createContext<IAppContext | null>(null)
+
 function App() {
 
     // 1 - Variaveis
@@ -25,52 +41,52 @@ function App() {
 
 
     // 2 - Funções
-    const userGreeting = (name: string): string =>{
+    const userGreeting = (name: string): string => {
         return `Olá, ${name}!`
     }
 
     // 8 - Types
     const myText: textOrNull = "Tem algum texto aqui"
     let mySecondText: textOrNull = null
-
     mySecondText = "Agora tem um valor"
 
+
     return (
-        <div className="App">
-            <h1>Seção 11 React com Typescript</h1>
-            <h2>Exibindo a variavel nome: {name}</h2>
-            <h2>Exibindo a variavel age: {age}</h2>
-            {isWorking && (
-                <p>Está trabalhando</p>
-            )}
-            <h2>Chamando função userGreeting: {userGreeting(name)}</h2>
-            <h2>Chamando componente FirstComponent</h2>
-            <FirstComponent/>
-            <h2>Chamando componente SecondComponent</h2>
-            <SecondComponent name={'Ollyver'} />
-            <h2>Chamando componente Destructuring</h2>
-            <Destructuring
-                title={'Título passado'}
-                content={'Content passado. Content passado.'}
-                comentQty={5}
-                tags={['tag1', 'tag2']}
-                category={Category.TS}
-            />
-            <h2>Chamando componente DestructuringComGenerics</h2>
-            <DestructuringComGenerics
-                title={'Título passado DestructuringComGenerics'}
-                content={'Content passado DestructuringComGenerics. Content passado DestructuringComGenerics.'}
-                comentQty={10}
-                tags={['python', 'react']}
-            />
-            <h2>Chamando componente State</h2>
-            <State/>
-            <p>Chamando type textOrNull - myText: {myText}</p>
-            <p>Chamando type textOrNull - mySecondText: {mySecondText}</p>
-            <p>Chamando type testandoFixed: {testandoFixed}</p>
-
-
-        </div>
+        <AppContext.Provider value={contextValue}>
+            <div className="App">
+                <h1>Seção 11 React com Typescript</h1>
+                <h2>Exibindo a variavel nome: {name}</h2>
+                <h2>Exibindo a variavel age: {age}</h2>
+                {isWorking && (
+                    <p>Está trabalhando</p>
+                )}
+                <h2>Chamando função userGreeting: {userGreeting(name)}</h2>
+                <h2>Chamando componente FirstComponent</h2>
+                <FirstComponent/>
+                <h2>Chamando componente SecondComponent</h2>
+                <SecondComponent name={'Ollyver'}/>
+                <h2>Chamando componente Destructuring</h2>
+                <Destructuring
+                    title={'Título passado'}
+                    content={'Content passado. Content passado.'}
+                    comentQty={5}
+                    tags={['tag1', 'tag2']}
+                    category={Category.TS}
+                />
+                <h2>Chamando componente DestructuringComGenerics</h2>
+                <DestructuringComGenerics
+                    title={'Título passado DestructuringComGenerics'}
+                    content={'Content passado DestructuringComGenerics. Content passado DestructuringComGenerics.'}
+                    comentQty={10}
+                    tags={['python', 'react']}
+                />
+                <h2>Chamando componente State</h2>
+                <State/>
+                <p>Chamando type textOrNull - myText: {myText}</p>
+                <p>Chamando type textOrNull - mySecondText: {mySecondText}</p>
+                <p>Chamando type testandoFixed: {testandoFixed}</p>
+            </div>
+        </AppContext.Provider>
     );
 }
 
